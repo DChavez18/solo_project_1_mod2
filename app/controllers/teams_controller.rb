ccec7b8543871dcb11b843ad6ebe18ec844e877b
Derek Chavez <frrenose17@yahoo.com>
2023-07-07 16:52:55 -0600

    feat: add show_players page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -6,4 +6,9 @@ class TeamsController < ApplicationController
   def show
     @teams = Team.find(params[:id])
   end
+
+  def show_players
+    @team = Team.find(params[:id])
+    @players = @team.players
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   get "/teams", to: "teams#index"
   get "/teams/:id", to: "teams#show"
+  get "/teams/:id/players", to: "teams#show_players"
   
   get "/players", to: "players#index"
   get "players/:id", to: "players#show"
