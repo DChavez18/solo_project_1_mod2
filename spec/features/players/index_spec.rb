1aff95c92d90359e87a5737218a2b13f46ee396a
Derek Chavez <frrenose17@yahoo.com>
2023-07-08 13:19:02 -0600

    feat: create player index link for each page

--- spec/features/players/index_spec.rb
+++ spec/features/players/index_spec.rb
@@ -22,4 +22,47 @@ RSpec.describe "player index" do
       expect(page).to have_content(player_1.team_id)
     end
   end
+
+# [ ] done
+# User Story 8, Child Index Link
+# As a visitor
+# When I visit any page on the site
+# Then I see a link at the top of the page that takes me to the Child Index
+
+  describe "When I visit any page on the site" do
+    describe "team page"do
+      it "has a link that connects to the players index" do
+
+        visit "/teams"
+        expect(page). to have_link("Player Index", href: "/players")
+      end
+    end
+
+    describe "welcome page" do
+      it "has a link that connects to the players index" do
+
+        visit "/"
+        expect(page).to have_link("Player Index", href: "/players")
+      end
+    end
+
+    describe "player show page" do
+      it "has a link that connects to the players index" do
+        team_1 = Team.create!(name: "Chiefs", city: "Kansas City", rank: 1, stadium: true, id: 1)
+        player_1 = Player.create!(name: "Patrick Mahomes", drafted: true, jersey_num: 15, team_id: 1)
+
+        visit "/players/#{player_1.id}"
+        expect(page).to have_link("Player Index", href: "/players")
+      end
+    end
+
+    describe "team show page" do
+      it "has a link taht connects to the players index" do
+        team_1 = Team.create!(name: "Chiefs", city: "Kansas City", rank: 1, stadium: true, id: 1)
+
+        visit "/teams/#{team_1.id}"
+        expect(page).to have_link("Player Index", href: "/players")
+      end
+    end
+  end
 end
